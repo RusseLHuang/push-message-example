c08830194c8be1826f74137e378af2a22c701cbe
Russel <>
2021-06-14 13:23:11 +0800

    apply client connection info to push registry for later invoek

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/RusseLHuang/push-message-example
 go 1.14
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/go-redis/redis/v8 v8.10.0
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/websocket v1.4.2
 )
--- server.go
+++ server.go
@@ -3,8 +3,10 @@ package main
 import (
 	"flag"
 	"log"
+	"net"
 	"net/http"
 
+	pushregistry "github.com/RusseLHuang/push-message-example/push_registry"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
 )
@@ -18,6 +20,19 @@ var clientConnMap map[string]Client
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 var upgrader = websocket.Upgrader{} // use default options
+var nodeIP string
+
+func setOutboundIP() {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	nodeIP = localAddr.IP.String()
+}
 
 func closeConn(deviceID string) {
 	clientConnMap[deviceID].conn.Close()
@@ -37,6 +52,8 @@ func connect(w http.ResponseWriter, r *http.Request) {
 		clientID: vars["clientID"],
 		conn:     c,
 	}
+
+	pushregistry.SetPersistentConnectionID(vars["clientID"], nodeIP)
 
 	defer closeConn(vars["clientID"])
 
@@ -75,13 +92,16 @@ func main() {
 	flag.Parse()
 	log.SetFlags(0)
 
+	setOutboundIP()
+	pushregistry.InitClientConnection()
+
 	clientConnMap = make(map[string]Client)
 
-	r := mux.NewRouter()
-	r.HandleFunc("/connect/{clientID}", connect)
-	r.HandleFunc("/sendMessage/{clientID}", sendMessage)
+	router := mux.NewRouter()
+	router.HandleFunc("/connect/{clientID}", connect)
+	router.HandleFunc("/sendMessage/{clientID}", sendMessage)
 
-	http.Handle("/", r)
+	http.Handle("/", router)
 
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
